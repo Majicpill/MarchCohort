e9d938e2e4fa01db3249df8c5a5f6ab221f2efc8
majicpill <muslimbydesignx@gmail.com>
2023-05-03 10:30:44 +0100

    JavaScript Fundamentals

---
+++ js/lesson2.js
@@ -0,0 +1,19 @@
+// Syntaxes
+
+console.log ("welcome to lesson 2")
+
+// variableType variable = variableAssignment
+var name = 0;
+let number = 1;
+const age = 0;
+
+// camelCase System
+var myNameIs = "AbdulMajid"
+
+console.log('inside the variable number is: ${number}')
+
+number = 25
+console.log('inside the variable number is: ${number}')
+
+// DataTypes
+" "
